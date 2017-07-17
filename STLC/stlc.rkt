a5e86ce98c9bc417a806972e147cfe050eae1b5c
migeed-z <migeed.z@outlook.com>
2017-07-17 10:44:14 -0400

    add env

--- STLC/stlc.rkt
+++ STLC/stlc.rkt
@@ -3,17 +3,41 @@
 
 
 (define-language STLC
-  (e ::= x (λ (x τ) e) (e e) integer)
+  (e ::= x (λ (x τ) e) (e e) v)
   (τ ::= Int (→ τ τ))
   (Γ ::= ((x τ) ...))
-  (x ::= variable-not-otherwise-mentioned))
+  (env ::= ((x v) ...))
+  (x ::= variable-not-otherwise-mentioned)
+  (v ::= integer ((λ (x) e) env)))
+
+; (lookup env x) retrieves x's value from env
+(define-metafunction STLC
+  [(lookup ((x_1 v_1) ... (x v) (x_2 v_2) ...) x)
+   v
+   (side-condition (not (member (term x) (term (x_1 ...)))))]
+   [(lookup ((x_2 v_2) ...) x) ;defined diff. in the tutorial why???
+     #f])
+
+;tests
+(test-equal (term (lookup ((x 3)) x)) (term 3))
+(test-equal (term (lookup ((y 3)) x)) #f)
+(test-equal (term (lookup () x)) #f)
+
+
+#|
+(define eval
+  (reduction-relation
+   STLC
+   (--> 
+        )))|#
+
+
 
 (define Γ? (redex-match? STLC Γ))
 
 (test-equal (Γ? (term [(x Int)])) #t)
 (test-equal (Γ? (term [(x (→ Int Int))])) #t)
 (test-equal (Γ? (term ((x Int) (x (→ Int Int))))) #t)
-
 
 (define-judgment-form STLC
   #:mode (infer-type I I O)
@@ -36,13 +60,11 @@
   [(where ((x_1 τ_1) ... (x τ) (x_2 τ_2) ...) Γ)
   ----------------------------------------------
    (infer-type Γ x τ)]
-
-  
   )
 
-(test-equal (judgment-holds (infer-type () 3 Int)) #t)
-(test-equal (judgment-holds (infer-type () (λ (x Int) x) (→ Int Int))) #t)
-(test-equal (judgment-holds (infer-type () ((λ (x Int) x) 3) Int)) #t)
+(test-judgment-holds (infer-type () 3 Int))
+(test-judgment-holds (infer-type () (λ (x Int) x) (→ Int Int)))
+(test-judgment-holds (infer-type () ((λ (x Int) x) 3) Int))
 
 
 ; (side-condition ,(printf "HI gamma is ~a~n" (term Γ_1)))
